f133092cfe6e227e7e2d57091e55712687806e5d
Gilad Silberberg <gsilberberg@championsoncology.com>
2025-06-10 11:37:27 +0000

    assertion for positive values

--- R/madvar.R
+++ R/madvar.R
@@ -1,13 +1,15 @@
 #' Filter biological data by variance, or find a cutoff
 #'
 #' @description This function calculates a cutoff based on data variance, aiming to filter out the "0 variance peak".
-#' @param data a numeric matrix or a numeric vector. If matrix - Genes must be in rows
+#' @param data a numeric matrix or a numeric vector (with positive values). If matrix - Genes must be in rows
 #' @param mads numeric (0-10]. Number of MADs to be added to the median for cutoff. Default=2 according to the outlier detection threshold. 
 #' @param must_genes a string vector with gene names that must be included in the output. Relevant only for matrix input.
 #' @param plot_density logical. Whether to plot the density of data variance distribution with the calculated cutoffs.
 #' @param ... additional arguments to other functions
 #'
-#' @return If `plot_density` == FALSE: a filtered matrix (if `data` is a matrix) or a numeric cutoff (if `data` is a vector); If `plot_density` == TRUE: a `ggplot` object.
+#' @return If `plot_density` == FALSE: a filtered matrix (if `data` is a matrix) or a numeric cutoff (if `data` is a vector); \cr
+#' If `plot_density` == TRUE: a `ggplot` object. The blue, gray and red vertical lines in the plot correspond to mode(Asselin estimate, bw=0.9), median, and final cutoff, respectively. \cr
+#' 
 #'
 #' @export
 #' @importFrom modeest asselin
@@ -21,6 +23,7 @@
 #'}
 madvar <- function (data, mads = 2, must_genes = NULL, plot_density = FALSE, ...) {
   assert_that(mads > 0 && mads <= 10, msg = "Error: 'mads' must be > 0 and <= 10")
+  assert_that(min(data, na.rm = T) >= 0, msg = "Error: Negative values are not allowed")
   abline <- axis <- par <- text <- NULL
   mat <- FALSE
   if (is.matrix(data) | is.data.frame(data))
