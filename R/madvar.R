4154c82a30b97a1d35a2a5651fa68fecd3a6603c
Gilad Silberberg <gsilberberg@championsoncology.com>
2025-06-23 11:30:04 +0000

    BIORD-772 added check and message for calculating and plotting mode

--- R/madvar.R
+++ R/madvar.R
@@ -2,14 +2,14 @@
 #'
 #' @description This function calculates a cutoff based on data variance, aiming to filter out the "0 variance peak".
 #' @param data a numeric matrix or a numeric vector (with positive values). If matrix - Genes must be in rows
-#' @param mads numeric (0-10]. Number of MADs to be added to the median for cutoff. Default=2 according to the outlier detection threshold. 
+#' @param mads numeric (0-10]. Number of MADs to be added to the median for cutoff. Default=2 according to the outlier detection threshold.
 #' @param must_genes a string vector with gene names that must be included in the output. Relevant only for matrix input.
 #' @param plot_density logical. Whether to plot the density of data variance distribution with the calculated cutoffs.
 #' @param ... additional arguments to other functions
 #'
 #' @return If `plot_density` == FALSE: a filtered matrix (if `data` is a matrix) or a numeric cutoff (if `data` is a vector); \cr
 #' If `plot_density` == TRUE: a `ggplot` object. The blue, gray and red vertical lines in the plot correspond to mode(Asselin estimate, bw=0.9), median, and final cutoff, respectively. \cr
-#' 
+#'
 #'
 #' @export
 #' @importFrom modeest asselin
@@ -49,13 +49,20 @@ madvar <- function (data, mads = 2, must_genes = NULL, plot_density = FALSE, ...
   median_peak <- 2 * med - min(gene_variance)
   if (plot_density) { # Exploratoration mode with plot
     assert_that(mat, msg = "Error: The plot can be generated only for a matrix/data frame")
-    ggplot(as.data.frame(gene_variance), aes(x=gene_variance)) +
+    mode <- asselin(gene_variance, ...)
+    p <- ggplot(as.data.frame(gene_variance), aes(x=gene_variance)) +
       geom_density(alpha=0.4) +
       labs(x = "Gene variance", y = "Density", ...) +
       theme_bw() +
       geom_vline(xintercept = med, color = "gray", linetype = "dashed") +
-      geom_vline(xintercept = asselin(gene_variance, ...), color = "blue", linetype = "dashed") +
       geom_vline(xintercept = med + MAD * mads, color = "red", linetype = "dashed")
+    if(is.numeric(mode)) {
+      p +
+        geom_vline(xintercept = mode, color = "blue", linetype = "dashed")
+    } else {
+      message("Could not calculate mode")
+      p
+    }
   } else {
     if (mat) {
       data[gene_variance >= med + MAD * mads | rownames(data) %in% must_genes, ]
